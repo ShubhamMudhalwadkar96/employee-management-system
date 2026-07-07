538ef394e955dbd0fcb70b794c4b38395e1d78ec
shubhamMudhalwadkar96 <shubham.mudhalwadkar96@gmail.com>
2026-07-07 12:50:13 +0530

    Added Swagger Implementation

---
+++ src/main/java/com/emp/ems/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.emp.ems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
