ac4b44f72c32370d9b4215b44ccb58e446a14ba3
shubhamMudhalwadkar96 <shubham.mudhalwadkar96@gmail.com>
2026-07-07 12:50:13 +0530

    Added Swagger Implementation

---
+++ src/main/java/com/emp/ems/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.emp.ems.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee Management Service API",
+                version = "v1",
+                description = "Enterprise Employee Management System built using Spring Boot 4 and Java 21",
+                contact = @Contact(
+                        name = "Shubham Mudhalwadkar",
+                        email = "shubhammd96@gmail.com"
+                )
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
